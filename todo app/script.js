7d7be87215fcd49019b5b55a7ec997188cd98ab6
Hunter Parks <parks.hunter1992@gmail.com>
2022-04-12 23:23:33 -0500

    made a quick little todo app!!

---
+++ todo app/script.js
@@ -0,0 +1,59 @@
+
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUL = document.getElementById("todos");
+
+
+form.addEventListener('submit', (e) =>{
+    e.preventDefault();
+
+    addToDo();
+});
+
+    function addToDo() {
+        const todoText = input.value;
+
+        if (todoText) {
+            const todoEl = document.createElement('li');
+            todoEl.innerText = todoText;
+
+            todoEl.addEventListener('click', () => {
+                todoEl.classList.toggle('completed')
+
+                updateLS();
+            });
+
+            todoEl.addEventListener('contextmenu', (e) => {
+                e.preventDefault();
+
+                todoEl.remove();
+
+                updateLS();
+            });
+
+            todosUL.appendChild(todoEl)
+
+            input.value = "";
+
+            updateLS();
+        }
+    }
+
+
+    function updateLS() {
+        const todosEl = document.querySelectorAll('li');
+
+        const todos = [];
+
+        todosEl.forEach((todoEl) => {
+            todos.push({
+                text: todoEl.innerText,
+                completed: todoEl.classList.contains("completed"),
+            });
+        });
+
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }
+
+
+
